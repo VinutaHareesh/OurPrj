b784a706a71d4e81f92c58031a449275de7f4d61
VinutaHareesh <158451996+VinutaHareesh@users.noreply.github.com>
2024-03-03 21:20:59 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import './App.css';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-// import Header from './componet/header';
-// import Footer from './componet/footer';
-import FirstComp from './componet/firstComp'
-//import logo from './logo.svg';
-import FroentPage from './pages/froentPage';
-import ComingSoon from './pages/comingSoon';
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Header from "./componet/header";
+import Footer from './componet/footer';
+import FroentPage from "./pages/froentPage";
+import ComingSoon from "./pages/comingSoon";
+import Soon from "./componet/Soon";
+import About from './pages/aboutUs';
+import { Container } from 'react-bootstrap';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-     <> 
-        {/* <Header></Header>
-
-        <Footer ></Footer> */}
-        {/* <FirstComp></FirstComp> */}
-        {/* <img src={logo} alt="My Logo" />  */}
-
-<FroentPage></FroentPage> 
-
-{/* <ComingSoon></ComingSoon> */}
-
+    <>
+    <Router>
+      <Header />
+      <Container fluid>
+        <Routes>
+          <Route path="/" element={<FroentPage />} />
+          <Route path="/AboutUs" element={<About />} />
+          {/* <Route path="/ComingSoon" element={<ComingSoon />} /> */}
+          <Route path="/ComingSoon" element={<Soon />} />          
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
     </>
   );
 }
